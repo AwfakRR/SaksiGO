335fc26669b68abff12151ba0d28d0958c7c16fc
javelinrain <ficky5050@gmail.com>
2022-08-11 13:52:58 +0700

    profile as legal advocate page is done

--- app/src/main/java/com/development/saksigo/ProfessionFeature/CompleteRegistration25Fragment.java
+++ app/src/main/java/com/development/saksigo/ProfessionFeature/CompleteRegistration25Fragment.java
@@ -1,13 +1,11 @@
 package com.development.saksigo.ProfessionFeature;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
@@ -23,7 +21,7 @@ public class CompleteRegistration25Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.profession_complete_registration_25_fragment, container, false);
 
-        spinnerLegalServices = (Spinner) root.findViewById(R.id.spinner_legalServices);
+        spinnerLegalServices = (Spinner) root.findViewById(R.id.spinner_Gender);
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.id_advocateType));
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
