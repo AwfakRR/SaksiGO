06f6ea34d9f548617924d66f60347f93a486aecc
javelinrain <ficky5050@gmail.com>
2022-08-15 17:42:13 +0700

    sneed

--- app/src/main/java/com/development/saksigo/PhotoProfileActivity.java
+++ app/src/main/java/com/development/saksigo/PhotoProfileActivity.java
@@ -1,22 +1,34 @@
 package com.development.saksigo;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
+import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -42,6 +54,50 @@ public class PhotoProfileActivity extends AppCompatActivity {
             imageUri = result.getUri();
 
             circleImageViewProfilePic.setImageURI(imageUri);
+        }else {
+            Toast.makeText(this, "Error, try again.", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+    private void uploadProfileImage(){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Set your profile");
+        progressDialog.setMessage("Please wait while we are setting your data");
+        progressDialog.show();
+
+        if (imageUri !=null){
+            final StorageReference fileRef = storageProfilePicsRef.child(mAuth.getCurrentUser().getUid()+ ".jpg");
+
+            uploadTask = fileRef.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+
+                    if (!task.isSuccessful()){
+                        throw task.getException();
+                    }
+                    return fileRef.getDownloadUrl();
+                }
+            }) .addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()){
+                        Uri downloadUrl = task.getResult();
+                        myUri = downloadUrl.toString();
+
+                        HashMap<String, Object> userMap = new HashMap<>();
+                        userMap.put("image", myUri);
+
+                        databaseReference.child(mAuth.getCurrentUser().getUid()).updateChildren(userMap);
+
+                        progressDialog.dismiss();
+                    }
+                }
+            });
+        }
+        else {
+            progressDialog.dismiss();
+            Toast.makeText(this, "Image not selected", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -75,5 +131,28 @@ public class PhotoProfileActivity extends AppCompatActivity {
             }
         });
 
+        getUserinfo();
+
+
+
+    }
+    private void getUserinfo() {
+        databaseReference.child(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0)
+                {
+                    if (dataSnapshot.hasChild("image")){
+                        String image = dataSnapshot.child("image").getValue().toString();
+                        Picasso.get().load(image).into(circleImageViewProfilePic);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/development/saksigo/ProfessionFeature/CompleteRegistration0Fragment.java
+++ app/src/main/java/com/development/saksigo/ProfessionFeature/CompleteRegistration0Fragment.java
@@ -9,7 +9,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -19,11 +21,15 @@ import com.development.saksigo.IntroductionActivity;
 import com.development.saksigo.PhotoProfileActivity;
 import com.development.saksigo.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
+import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
 
 
@@ -31,7 +37,11 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CompleteRegistration0Fragment extends Fragment {
 
+    private CircleImageView profileImageView;
+    private DatabaseReference databaseReference;
 
+    TextView textViewFullname;
+    FirebaseAuth mAuth;
 
     CircleImageView circleImageViewProfilePic;
 
@@ -39,10 +49,16 @@ public class CompleteRegistration0Fragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.profession_complete_registration_0_fragment, container, false);
-
+        mAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("User");
         circleImageViewProfilePic = root.findViewById(R.id.circleImageView_profilePhoto);
+        textViewFullname = root.findViewById(R.id.textView_fullnameProfession);
+
+
 
         Intent intent = new Intent(container.getContext(), PhotoProfileActivity.class);
+
+        getUserInfo();
 
         circleImageViewProfilePic.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,5 +70,30 @@ public class CompleteRegistration0Fragment extends Fragment {
         });
 
         return root;
+    }
+
+    private void getUserInfo() {
+        databaseReference.child(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0){
+                    String name = dataSnapshot.child("fullname").getValue().toString();
+
+                    textViewFullname.setText(name);
+
+                    if(dataSnapshot.hasChild("image")){
+                        String image = dataSnapshot.child("image").getValue().toString();
+                        Picasso.get().load(image).into(profileImageView);
+                    }
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
