08bcfc08229deaa6139af27f20874e4203816b2c
hendy <fishintank@protonmail.com>
2022-08-16 15:50:01 +0700

    profession registration layout almost done

--- app/src/main/java/com/development/saksigo/ProfessionFeature/CompleteRegistration25Fragment.java
+++ app/src/main/java/com/development/saksigo/ProfessionFeature/CompleteRegistration25Fragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
@@ -15,13 +16,18 @@ import com.development.saksigo.R;
 
 public class CompleteRegistration25Fragment extends Fragment {
 
+    String stringAboutYou, stringVideoLink, stringLegalServices;
+    EditText editTextAboutYou, editTextVideoLink;
     Button buttonSaveContinue;
     Spinner spinnerLegalServices;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.profession_complete_registration_25_fragment, container, false);
 
-        spinnerLegalServices = (Spinner) root.findViewById(R.id.spinner_Gender);
+
+        editTextAboutYou = (EditText) root.findViewById(R.id.editText_aboutYouProfession);
+        editTextVideoLink = (EditText) root.findViewById(R.id.editText_videoLinkProfession);
+        spinnerLegalServices = (Spinner) root.findViewById(R.id.spinner_LegalServices);
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.id_advocateType));
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -35,6 +41,12 @@ public class CompleteRegistration25Fragment extends Fragment {
         buttonSaveContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                stringAboutYou = editTextAboutYou.getText().toString();
+                stringVideoLink = editTextVideoLink.getText().toString();
+                stringLegalServices = spinnerLegalServices.getSelectedItem().toString();
+
+
 
 
                 fragmentTransaction.replace(R.id.containerCompleteRegistration, completeRegistration50Fragment);
